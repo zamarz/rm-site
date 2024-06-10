c5e151c924c70459130e75424c42620a21ef1b24
zamarz <zachmarzouk@gmail.com>
2024-06-10 10:44:09 +0100

    Add initial pages

---
+++ components/Match.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Match = () => {
+  return (
+    <div>
+        <div>
+            What you are: Creative, Proactive, Passionate, Transparent, Laid Back, Welcoming, Collaborative, Nerdy
+        </div>
+    </div>
+  )
+}
+
+export default Match
---
+++ components/WhyMe.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const WhyMe = () => {
+  return (
+    <div>
+        <div>
+            <p>
+            Passionate about software development and technology
+            </p>
+            <p>Need to talk with internal team and clients</p>
+            <p>Need to work in a team</p>
+            <p>Can organise own resources and coordinate with external parties</p>
+            <p>Need to proactively learn new skills and keep up-to-date with the latest advances and trends in technology</p>
+        </div>
+    </div>
+  )
+}
+
+export default WhyMe
