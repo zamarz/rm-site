d6cf020011831fd7988da8710fb021de00b32e2c
zamarz <zachmarzouk@gmail.com>
2024-06-11 09:31:31 +0100

    Swapped order of components

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,14 @@ import Intro from "@/components/Intro";
 import Match from "@/components/Match";
 import Navbar from "@/components/Navbar";
 import Skills from "@/components/Skills";
-import WhyMe from "@/components/WhyMe";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Navbar />
       <Intro />
-      <WhyMe />
-      <Match />
       <Skills />
+      <Match />
       <Extra />
     </main>
   );
