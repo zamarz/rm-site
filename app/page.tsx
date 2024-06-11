d16f56bf6d300562b6f7df4ad2efcade9ab40aa9
zamarz <zachmarzouk@gmail.com>
2024-06-11 10:52:29 +0100

    Remove nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Intro from "@/components/Intro";
 import Match from "@/components/Match";
-import Navbar from "@/components/Navbar";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Navbar />
       <Intro />
       <Skills />
       <Match />
