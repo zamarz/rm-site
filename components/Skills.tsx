c855cc4454f257c5aef7fa7161e37ee1e8aa36a7
zamarz <zachmarzouk@gmail.com>
2024-06-11 09:17:38 +0100

    Add more info to skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,12 +3,16 @@ const Skills = () => {
   return (
     <section className="px-10 py-10 mx-auto container min-h-screen">
     <div className="py-10 mx-10 px-10">
-        <div className="flex flex-row items-center py-5 ">What do I know?
-            </div>
-            <div>
-                <p>I studied at a bootcamp which equipped me with...</p>
-                <p>Since graduating I have been learning..</p>
-                <p>Want to see my work? Head to my portfolio and find links to my project live sites and code </p>
+        <div className="flex flex-row items-center py-5 ">
+          <h2 className="font-black text-5xl py-5">What do I know?</h2>
+            
+            <ul>
+              <li>I've also been part of a number of agile teams, in the UK and abroad, and know how to make full use of collaboration tech like Slack, Jira, and Trello. I have great interpersonal and communication skills after working as a journalist, communications consultant and copywriter for a number of years.
+              </li>
+                <li>I studied at a bootcamp which equipped me with eseential skills including proficiency in JavaScript, React, Jest, Git and GitHub...and much more!</li>
+                <li>Since graduating I have been learning refining my skills in personal projects and learning new things like TypeScript and Next.js.</li>
+                <li>Want to see my work? Head to my portfolio and find links to my <a className="underline" href="https://www.zachmarzouk.dev/"> project live sites </a> and delicious code.</li>
+            </ul>
             </div>
             </div>
             </section>
